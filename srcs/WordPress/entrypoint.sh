f5f5004dc27e593d73186f9a20ebda2e206c203c
Aykrifa <aykrifa@student.42.fr>
2026-05-24 20:31:46 +0200

    Work in progress, quick start block at this command: /wpclidemo.dev #  wp config create --dbname=wpclidemo --dbuser=root --prompt=dbpass 1/12 [--dbpass=<dbpass>]: adm wp config create --dbname='wpclidemo' --dbuser='root' --dbpass='adm' --dbhost='localhost' --dbprefix='wp_' --dbcharset='utf8' --dbcollate='' Error: Database connection error (2002) No such file or directory

--- srcs/WordPress/entrypoint.sh
+++ srcs/WordPress/entrypoint.sh
@@ -4,4 +4,10 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp --info
+# PHP Fatal error:  Allowed memory size of 134217728 bytes exhausted (tried to allocate 36864 bytes) in phar:///usr/local/bin/wp/vendor/wp-cli/wp-cli/php/WP_CLI/Extractor.php on line 100
+# Increase memory limit for extraction
+RUN echo "memory_limit=256M" >> /etc/php83/php.ini
+# Tuto quick start wp
+wp core download --path=wpclidemo.dev
+
 tail -f /dev/null
