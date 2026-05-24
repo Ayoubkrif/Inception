ac13bb400d8c32c5d6870a7b2808f9ae958570f1
Aykrifa <aykrifa@student.42.fr>
2026-05-24 20:21:11 +0200

    WP installation complete need to confif first website with https://make.wordpress.org/cli/handbook/guides/quick-start/

--- srcs/WordPress/entrypoint.sh
+++ srcs/WordPress/entrypoint.sh
@@ -2,6 +2,6 @@
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/wp
 wp --info
 tail -f /dev/null
