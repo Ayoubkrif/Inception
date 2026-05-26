70aa5159dfa61ff5c3fe7f93e42b6a023921bd47
Aykrifa <aykrifa@student.42.fr>
2026-05-26 10:18:07 +0200

    Work in progress word press

--- srcs/WordPress/docker-entrypoint.sh
+++ srcs/WordPress/docker-entrypoint.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
 wp --info
 #FIX: PHP Fatal error:  Allowed memory size of 134217728 bytes exhausted (tried to allocate 36864 bytes) in phar:///usr/local/bin/wp/vendor/wp-cli/wp-cli/php/WP_CLI/Extractor.php on line 100
 #NOTE: Increase memory limit for extraction
-RUN echo "memory_limit=256M" >> /etc/php83/php.ini
+echo "memory_limit=256M" >> /etc/php83/php.ini
 # Tuto quick start wp
 wp core download --path=wpclidemo.dev
 
