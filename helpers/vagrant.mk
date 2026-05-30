e03bf32be71f92450014a75dd9ed479d281890e6
Aykrifa <aykrifa@student.42.fr>
2026-05-30 11:59:19 +0200

    No more ssh unsafe copy

--- helpers/vagrant.mk
+++ helpers/vagrant.mk
@@ -3,14 +3,10 @@
 # **************************************************************************** #
 vm: # build vm with vagrant
 	VBoxManage setproperty machinefolder $$(pwd)
-	cp ~/.ssh/vm .
-	cp ~/.ssh/vm.pub .
 	vagrant up
 	vagrant ssh
 
 destroy: # vagrant destroy properly
-	rm -f vm
-	rm -f vm.pub
 	ssh-keygen -R "[localhost]:2222"
 	vagrant destroy -f
 
--- provision.sh
+++ provision.sh
@@ -10,12 +10,6 @@ apk add make curl git
 #INFO: Docker
 apk add docker docker-cli docker-cli-compose
 
-#WARN: Clefs SSH pour root
-mkdir -p /root/.ssh
-cp /shared/vm     /root/.ssh/id_rsa
-cp /shared/vm.pub /root/.ssh/id_rsa.pub
-chmod 600 /root/.ssh/id_rsa
-
 #INFO: Docker au démarrage + autoriser vagrant à l'utiliser
 rc-update add docker default
 service docker start
