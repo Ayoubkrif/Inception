7635cb26e6f76c568d74fce6cfd39caffe66fa88
Aykrifa <aykrifa@student.42.fr>
2026-06-04 18:10:58 +0200

    Portainer credentials

--- helpers/credential.mk
+++ helpers/credential.mk
@@ -37,6 +37,10 @@ env: # build .env
 		echo "HOST_WP_PATH=$$HOME/WP" >> .env; \
 		echo "WP_FILES_LOCATION=/var/www/html" >> .env; \
 		echo "DOMAIN_NAME=aykrifa.42.fr" >> .env; \
+		# BONUS
+		read -p "PORTAINER_ADMIN_LOGIN=" portainerlogin; \
+		echo "PORTAINER_ADMIN_LOGIN=$$portainerlogin" >> .env; \
+		# END OF BONUS
 	else \
 		printf ".env already exist — écraser ? [y/N] : "; \
 		read confirm; \
@@ -72,6 +76,14 @@ secrets: # build passwd
 		echo "$$userpass" > mariadb_password.secret; \
 		echo "$$wpadminpass" > wp_admin_password.secret; \
 		echo "$$wpuserpass" > wp_user_password.secret; \
+		# BONUS
+		printf "Mot de passe ADMIN Portainer : "; \
+		stty -echo; \
+		read portainerpass; \
+		stty echo; \
+		echo ""; \
+		echo "$$portainerpass" > portainer_admin_password.secret; \
+		# END OF BONUS
 		echo ""; \
 		echo "$(YELLOW)⚠ Secrets modifiés — lance 'make restart' pour re-provisionner la DB$(RESET)"; \
 	else \
