340b3068dba2d6f97b97d07a74a4d1810a7b1d56
Aykrifa <aykrifa@student.42.fr>
2026-06-04 18:07:15 +0200

    Mariadb fix, better heath check + alpine version of image + sh version of entrypoint

--- srcs/MariaDB/healthcheck.sh
+++ srcs/MariaDB/healthcheck.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-mariadb-admin --socket=/run/mysqld/mysqld.sock --user=healthcheck ping --silent
+mariadb --skip-ssl --protocol=socket \
+    -u root -p"$(cat /run/secrets/mariadb_root_password)" \
+    -e "SELECT 1 FROM information_schema.ENGINES WHERE engine='InnoDB' AND support IN ('YES','DEFAULT')" \
+    >/dev/null 2>&1
